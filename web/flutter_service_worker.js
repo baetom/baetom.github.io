2ab957b256e3f78a16d6254fa4e858c23996ce30
baetom <ifunbae@gmail.com>
2022-12-21 13:05:10 +0900

    aa

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "43cc5c8a962ede693f51e3c116a8335f",
-"index.html": "b01e1fa5f20c89405b854cd52b4e9298",
-"/": "b01e1fa5f20c89405b854cd52b4e9298",
+"index.html": "71bcfe3b3585f0d5b0c459c541689426",
+"/": "71bcfe3b3585f0d5b0c459c541689426",
 "main.dart.js": "b66e381769d0f950d7cebe85a0e439ca",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
